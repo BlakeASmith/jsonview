229b960d5d1195add3431794dbc8a469f5576f1e
= <blake.woodsmith@gmail.com>
2024-06-09 22:20:19 -0700

    refactor: encapsulate block and indent behaviour in component, move to inline styles

--- index.jsx
+++ index.jsx
@@ -1,147 +1,108 @@
 /* eslint-disable react/prop-types */
-import "./jsonview.css"
-import { useState } from 'react'
+import { useState } from "react";
 
-/**
- * @typedef {
- *   value: object
- *   depth: int
- * } JsonValueProps
- */
-
-function useCollapse(depth) {
-  const [curDepth, setDepth] = useState(depth)
-
-  const show = () => {
-    setDepth(1)
-  }
-  const hide = () => {
-    setDepth(curDepth - 1)
-  }
-
-  return [curDepth > 0, show, hide]
+const THEME = {
+	bgColor: "black",
+	keyColor: "lightblue",
+	stringValueColor: "lightpink",
+	literalValueColor: "orange",
+	slightIndent: "5px",
+	indent: 10,
+	indentUnit: "px"
 }
 
-function JsonKey({ keyName }) {
-  return <span className="json-key">&quot;{keyName}&quot;</span>
-}
+function Block({ depth, openchar, closechar, indent, children }) {
+  const [curDepth, setDepth] = useState(depth);
+  const show = () => setDepth(1);
+  const hide = () => setDepth(curDepth - 1);
 
-function StringValue({ value }) {
-  return <span className="json-value "> &quot;{value}&quot;</span>
-}
-
-function NumberValue({ value }) {
-  return <span className="json-value-number "> {value}</span>
-}
-
-function BooleanValue({ value }) {
-  return <span className="json-value-bool "> {value ? 'true' : 'false'}</span>
-}
-
-function UndefinedValue() {
-  return <span className="json-value-number "> undefined</span>
-}
-
-function ObjectValue({ value, isRoot, depth }) {
-  const upperIndentLevel = isRoot === true ? 'indent-slight' : 'indent-slight'
-  const bodyIndentLevel = isRoot === true ? 'indented-1' : 'indented-2'
-  const lowerIndentlevel = isRoot === true ? 'indent-slight' : 'indented-1'
-
-  const [shouldShow, show, hide] = useCollapse(depth)
+  const indentAmount = `${THEME.indent * indent}${THEME.indentUnit}`
 
   return (
     <>
-      {shouldShow ? (
+      {curDepth > 0 ?
         <>
-          <span className={upperIndentLevel} onClick={hide}>
-            &#123;<span className="indented-slight">-</span>
+          <span onClick={hide}>
+            {openchar}
+            <span style={{paddingLeft: THEME.slightIndent}}>-</span>
           </span>
-          <div className={bodyIndentLevel}>
-            {Object.entries(value).map(([k, v]) => (
-              <JsonKV key={k} keyName={k} value={v} depth={depth - 1} />
-            ))}
-          </div>
-          <span className={lowerIndentlevel}>&#125;</span>
+		  <div style={{paddingLeft: indentAmount}}>
+          	{children}
+		  </div>
+          <span>{closechar}</span>
         </>
-      ) : (
-        <span className="elipsis" onClick={show}>
-          &#123; ... &#125;
+       : 
+        <span onClick={show}>
+          {openchar} ... {closechar}
         </span>
-      )}
+      }
     </>
-  )
+  );
 }
-
-function ArrayValue({ value, depth }) {
-  const [shouldShow, show, hide] = useCollapse(depth)
+function ObjectValue({ value, indent, depth }) {
   return (
-    <>
-      {shouldShow ? (
-        <>
-          <span className="indented-slight" onClick={hide}>
-            &#91; <span className="indented-slight">-</span>
-          </span>
-          {[...value].map((v) => (
-            // eslint-disable-next-line react/jsx-key
-            <div className="indented-1">
-              <span className="indented-1">
-                <JsonValue key={v} value={v} depth={depth - 1} />
-              </span>
-              <span>,</span>
-            </div>
-          ))}
-          <span className="indented-1">&#93;</span>
-        </>
-      ) : (
-        <span className="indented-slight elipsis" onClick={show}>
-          &#91; ... &#93;
-        </span>
-      )}
-    </>
-  )
+    <Block depth={depth} openchar="{" closechar="}" indent={indent}>
+      <div>
+        {Object.entries(value).map(([k, v]) => (
+          <JsonKV key={k} keyName={k} value={v} depth={depth - 1} indent={indent + 1} />
+        ))}
+      </div>
+    </Block>
+  );
 }
 
-function JsonValue({ value, isRoot, depth }) {
-  if (value === null) {
-    return <UndefinedValue></UndefinedValue>
+function ArrayValue({ value, depth, indent}) {
+  return (
+    <Block depth={depth} openchar="[" closechar="]" indent={indent}>
+      {[...value].map((v) => (
+        <div>
+          <JsonValue key={v} value={v} depth={depth - 1} indent={indent}/>
+          <span>,</span>
+        </div>
+      ))}
+    </Block>
+  );
+}
+
+function JsonValue({ value, indent = 1, depth = 1 }) {
+  if (value === null || value === undefined) {
+    return <span style={{color: THEME.literalValueColor}}> undefined</span>;
   }
 
   switch (typeof value) {
-    case 'string':
-      return <StringValue value={value} />
-    case 'number':
-      return <NumberValue value={value} />
-    case 'boolean':
-      return <BooleanValue value={value} />
-    case 'undefined':
-      return <UndefinedValue />
-    case 'object':
-      if (value instanceof Array) return <ArrayValue value={value} isRoot={isRoot} depth={depth} />
-      else return <ObjectValue value={value} isRoot={isRoot} depth={depth} />
-    case 'function':
-      return <p>Not Implemented</p>
+    case "string":
+      return <span style={{color: THEME.stringValueColor}}> &quot;{value}&quot;</span>;
+    case "number":
+      return <span style={{color: THEME.literalValueColor}}> {value}</span>;
+    case "boolean":
+      return (
+        <span style={{color: THEME.literalValueColor}}> {value ? "true" : "false"}</span>
+      );
+    case "object":
+      if (value instanceof Array) {
+        return <ArrayValue value={value} indent={indent} depth={depth} />;
+      } else {
+        return <ObjectValue value={value} indent={indent} depth={depth} />;
+      }
   }
 }
 
-function JsonKV({ keyName, value, depth }) {
+function JsonKV({ keyName, value, depth = 1, indent = 1 }) {
   return (
-    <>
-      <div className="json-kv">
-        <JsonKey keyName={keyName} />:
-        <JsonValue value={value} depth={depth} />
-      </div>
-    </>
-  )
+    <div>
+      <span style={{color: THEME.keyColor}}>&quot;{keyName}&quot;</span>
+      <span style={{ paddingRight: "5px" }}>:</span>
+      <JsonValue value={value} depth={depth} indent={indent} />
+    </div>
+  );
 }
 
-function JsonContainer({ children }) {
-  return <div className="json-object">{children}</div>
-}
-
-export default function JsonView({ data, depth=0 }) {
+export default function JsonView({ data, depth = 0 }) {
   return (
-    <JsonContainer>
-      <JsonValue type={'source'} value={data} isRoot={true} depth={depth}></JsonValue>
-    </JsonContainer>
+	<div style={{background: THEME.bgColor, borderRadius: "9px", padding: "5px"}}>
+      <JsonValue
+        value={data}
+      ></JsonValue>
+	</div>
   )
 }
